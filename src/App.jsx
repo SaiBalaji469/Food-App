3200638b844a349996abc2fd43fe013539962e1a
Balaji <balajijayavarapu.469@gmail.com>
2024-04-20 11:31:26 +0530

    React and TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import Hero from "./components/Hero"
 import HeadlineCards from "./components/HeadlineCards"
 import Navbar from "./components/Navbar"
 import Food from "./components/Food"
-import Category from "./components/Category"
+import Category
+ from "./components/Category"
 import React from "react"
+import Footer from "./components/Footer.jsx"
+
 
 
 function App() {
@@ -14,6 +17,8 @@ function App() {
         <HeadlineCards></HeadlineCards>
         <Food></Food>
         <Category></Category>
+        <Footer></Footer>
+
         </>
     )
   
---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import "/public/Footer.css"
+
+const Footer = () => <footer className="page-footer font-small blue pt-4">
+    <div className="container-fluid text-center text-md-left bg-gray-200 py-5 text-l max-w-[1640px] m-auto">
+        <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+            <h3 className='px-4 text-2xl sm:text-3xl md:text-4xl lg:5xl font-bold'>The <span className='text-orange-500'>Best</span></h3>
+                <h3 className='px-4 text-2xl sm:text-3xl md:text-4xl lg:5xl font-bold'><span className='text-orange-500'>Foods</span> Delivered</h3>
+                <p>"Celebrating the artistry of culinary craftsmanship."</p>
+            </div>
+
+            <hr className="clearfix w-100 d-md-none pb-0"/>
+
+            <div className="col-md-3 mb-md-0 mb-3">
+            <ul class="sc-kpKSZj eiDsBf">
+                <li class="sc-ijDOKB dzmwgu">
+                    <div class="sc-aXZVg gDLELL text-orange-500 text-2xl font-bold">Company</div></li>
+                    <li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/about" target="_blank"><div class="sc-aXZVg JTfrR">About</div></a></li><li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/careers" target="_blank"><div class="sc-aXZVg JTfrR">Careers</div></a></li><li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/team" target="_blank"><div class="sc-aXZVg JTfrR">Team</div></a></li><li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/swiggy-super" target="_blank" rel="nofollow noopener"><div class="sc-aXZVg JTfrR">Swiggy One</div></a></li><li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/instamart" target="_blank" rel="nofollow noopener"><div class="sc-aXZVg JTfrR">Swiggy Instamart</div></a></li><li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/swiggy-genie" target="_blank" rel="nofollow noopener"><div class="sc-aXZVg JTfrR">Swiggy Genie</div></a></li></ul>
+            </div>
+
+            <div className="col-md-3 mb-md-0 mb-3">
+              
+                <ul class="sc-kpKSZj eiDsBf"><li class="sc-ijDOKB dzmwgu"><div class="sc-aXZVg gDLELL text-orange-500 text-2xl font-bold">We deliver to:</div></li><li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/city/bangalore" class="sc-lgjHQU eCniLF"><div class="sc-aXZVg JTfrR">Bangalore</div></a></li><li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/city/gurgaon" class="sc-lgjHQU eCniLF"><div class="sc-aXZVg JTfrR">Gurgaon</div></a></li><li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/city/hyderabad" class="sc-lgjHQU eCniLF"><div class="sc-aXZVg JTfrR">Hyderabad</div></a></li><li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/city/delhi" class="sc-lgjHQU eCniLF"><div class="sc-aXZVg JTfrR">Delhi</div></a></li><li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/city/mumbai" class="sc-lgjHQU eCniLF"><div class="sc-aXZVg JTfrR">Mumbai</div></a></li><li class="sc-lfYzqA gbbLxt"><a href="https://www.swiggy.com/city/pune" class="sc-lgjHQU eCniLF"><div class="sc-aXZVg JTfrR">Pune</div></a></li><li class="sc-lfYzqA gbbLxt"><button class="sc-eBwKMn fNcbWI"><div class="sc-bHnlcS dBBaRE"><div class="sc-aXZVg ipOywu">589 cities</div><div class="sc-cKXybt fYKDAQ"><svg aria-hidden="true" height="12" width="12" class="sc-dhKdcB iVFUwp"><use xlink:href="/core/sprite-2e61ee4e.svg#chevronDown12"></use></svg></div></div></button></li></ul>
+            </div>
+        </div>
+    </div>
+
+
+</footer>
+
+export default Footer
